b4aa1fea9d323109da63a788380325a158ec4fb4
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-23 01:04:59 +0300

    add Tasks

---
+++ T34/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными
+//трёхзначными числами. Напишите программу, которая покажет количество 
+//чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.Clear();
+int[] numbers = new int[10];
+
+void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ColPositive(int[] array)
+{
+    int col = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1)
+        {
+            col++;
+        }
+    }
+    return col;
+}
+
+FillArray(numbers, 100, 1000);
+WriteArray(numbers);
+Console.WriteLine();
+
+int col = ColPositive(numbers);
+Console.WriteLine($"Количество чётных чисел в массиве: {col}");
---
+++ T35/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+
+Console.Clear();
+int[] array = new int[123];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 500);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > 9 & array[j] < 100)
+        count++;
+}
+Console.WriteLine();
+Console.WriteLine($"Массив содержит следующее количество элементов из отрезка [10,99]: {count} ");
+Console.WriteLine();
